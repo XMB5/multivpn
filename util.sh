0ca316aed78967815ad1d89641d610abcafda5e2
Sam Foxman <samfoxman320@gmail.com>
2019-01-20 13:32:11 -0500

    Add license

--- util.sh
+++ util.sh
@@ -1,3 +1,18 @@
+# This file is part of MultiVPN.
+#
+# MultiVPN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 netmask_to_netbits() {
     #https://github.com/OpenVPN/openvpn/blob/14d7e0e496f15563005fffc6d4791a95444ddf23/src/openvpn/route.c#L4041
     #function returns prefix for given netmask in arg1
